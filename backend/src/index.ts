2ede9ee62d1ae671e640e430eff421ae3e99e000
baothiquoc <bquoc3002@gmail.com>
2024-05-05 22:53:30 +0700

    update 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("connected to database"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const app = express();
 app.use(express.json());
@@ -15,6 +23,7 @@ app.get("/health", async (req: Request, res: Response) => {
 })
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
