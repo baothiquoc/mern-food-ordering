ef9cea7b4ebcdea3c65bfb93d3a75328865ac92e
baothiquoc <bquoc3002@gmail.com>
2024-05-06 22:35:02 +0700

    update

---
+++ backend/src/routes/RestaurantRoute.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controller/RestaurantController";
+
+const router = express.Router();
+
+// /api/restaurant/search/london
+router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"), RestaurantController.searchRestaurants);
+
+export default router;
